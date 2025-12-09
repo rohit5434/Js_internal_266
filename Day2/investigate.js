968b4f0800285c21edbe40707d20294782353d85
Rohit Mishra <147356228+rohit5434@users.noreply.github.com>
2025-12-09 05:30:13 +0000

    taskdone

---
+++ Day2/investigate.js
@@ -0,0 +1,73 @@
+//  let a = 23;
+//  const b ="abc";
+// var c = 78;
+//  d = 88;
+
+
+// console.log("initial values");
+// console.log("a = " +a);
+// console.log("b = " +b );
+// console.log("c = "  +c);
+// console.log("d = "  +d);
+
+//task 2
+
+//  a = 2;
+//  b ="abcd";
+//  c = 7;
+//  d = 18;
+
+
+// console.log("changed values");
+// console.log("a = " +a);
+// console.log("b = " +b );
+// console.log("c = "  +c);
+// console.log("d = "  +d);
+
+//task 3 
+
+//   let a = 213;
+//  const b ="abbc";
+// var c = 178;
+//  d = 288;
+
+// console.log("Redeclared variables + changed values");
+// console.log("a = " +a);
+// console.log("b = " +b );
+// console.log("c = "  +c);
+// console.log("d = "  +d);
+
+
+// datatypes
+
+// let a = "abc";
+// let b =" ";
+// let c = null;
+// let d;
+
+
+// console.log("initial values");
+// console.log("a = " +a);
+// console.log("b = " +b );
+// console.log("c = "  +c);
+// console.log("d = "  +d);
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+
+       
+              //
+
+// let score="33";
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let ok=Number(score)
+// console.log(typeof(ok));
+
+// let v1 = "abc123";
+// let v2 = Number(v1);
+// console.log(typeof v2);
