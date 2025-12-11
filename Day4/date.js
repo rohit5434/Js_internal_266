76c05f111b381589382b6db559f7290a688f1e49
Rohit Mishra <147356228+rohit5434@users.noreply.github.com>
2025-12-11 05:47:55 +0000

    day4

---
+++ Day4/date.js
@@ -0,0 +1,7 @@
+let date = new Date();
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.toLocaleDateString());
+console.log(date.toTimeString());
